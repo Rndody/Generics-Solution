09fcf93daa8c68698212a6998a11e62a9cb02bd7
rndody <rndody@gmail.com>
2025-02-15 22:13:02 +0200

    Add project files.

---
+++ Generics/Assignment 01/ArrayUtils.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics.Assignment_01
+{
+    static class ArrayUtils
+    {
+        /* Generic method ReverseArray<T>(T[] array) that reverses the order of elements in an array.
+          * Generic method FindMax<T>(T[] array)that finds the maximum element in an array.            */
+
+
+        public static void ReverseArray<T>(T[] array)
+        {
+            T[] reversedArray = new T[array.Length];
+                int index = 0;
+            for (int i = array.Length - 1; i >= 0; i--)
+            {
+                reversedArray[index] = array[i];
+                index++;
+            }
+            reversedArray.CopyTo(array, 0);
+        }
+        public static void FindMax<T>(T[] array)
+        {
+
+        }
+    }
+}
---
+++ Generics/Program.cs
@@ -0,0 +1,26 @@
+﻿using Generics.Assignment_01;
+
+namespace Generics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            #region ArrayUtils Method
+            int[] arr = { 1, 2, 3, 5, 3, 6, 9, 8 };
+
+            ArrayUtils.ReverseArray(arr);
+
+            for (int i = 0; i < arr.Length; i++) Console.WriteLine(arr[i]);
+
+            #endregion
+
+
+
+
+
+        }
+    }
+}
